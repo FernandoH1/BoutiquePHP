dbbbe0be99d9e71edd913d414c8272e84bf6ad3e
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-05-17 23:43:10 -0300

    Registro css hecho

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,80 +1,97 @@
-@extends('layouts.app')
+@extends('layouts.main', ['class' => 'off-canvas-sidebar', 'activePage' => 'register', 'title' => __('Material Dashboard')])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="container" style="height: auto;">
+    <div class="row align-items-center">
+        <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+            <form class="form" method="POST" action="{{ route('register') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
+                <div class="card card-login card-hidden mb-3">
+                    <div class="card-header card-header-primary text-center" style="background: linear-gradient(60deg, #ffde61, #f7ab9f,#ffde61)">
+                        <h4 class="card-title"><strong>{{ __('Registrarse') }}</strong></h4>
+                        <div class="social-line">
+                        <img src="{{ asset('/img/registro.png') }}" alt="HSS" height="100px" width="100px">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
+                    </div>
+                    <div class="card-body ">
+                        <!-- <p class="card-description text-center">{{ __('Or Be Classical') }}</p> -->
+                        <div class="bmd-form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="material-icons">face</i>
+                                    </span>
+                                </div>
+                                <input type="text" name="name" class="form-control" placeholder="{{ __('Name...') }}" value="{{ old('name') }}" required>
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
+                            @if ($errors->has('name'))
+                            <div id="name-error" class="error text-danger pl-3" for="name" style="display: block;">
+                                <strong>{{ $errors->first('name') }}</strong>
                             </div>
+                            @endif
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="material-icons">email</i>
+                                    </span>
+                                </div>
+                                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="mobile" class="col-md-4 col-form-label text-md-end">{{ __('Mobile') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile" autofocus>
-
-                                @error('mobile')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
+                            @if ($errors->has('email'))
+                            <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
+                                <strong>{{ $errors->first('email') }}</strong>
                             </div>
+                            @endif
                         </div>
-
+                        <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="material-icons">lock_outline</i>
+                                    </span>
+                                </div>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" required>
+                            </div>
+                            @if ($errors->has('password'))
+                            <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </div>
+                            @endif
+                        </div>
+                        <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="material-icons">lock_outline</i>
+                                    </span>
+                                </div>
+                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Confirm Password...') }}" required>
+                            </div>
+                            @if ($errors->has('password_confirmation'))
+                            <div id="password_confirmation-error" class="error text-danger pl-3" for="password_confirmation" style="display: block;">
+                                <strong>{{ $errors->first('password_confirmation') }}</strong>
+                            </div>
+                            @endif
+                        </div>
+                        <!-- ESTE -->
+                        <div class="bmd-form-group{{ $errors->has('mobile') ? ' has-danger' : '' }} mt-3">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="material-icons">smartphone</i>
+                                    </span>
+                                </div>
+                                <input type="text" name="mobile" class="form-control" placeholder="{{ __('Mobile...') }}" value="{{ old('mobile') }}" required>
+                            </div>
+                            @if ($errors->has('mobile'))
+                            <div id="mobile-error" class="error text-danger pl-3" for="mobile" style="display: block;">
+                                <strong>{{ $errors->first('mobile') }}</strong>
+                            </div>
+                            @endif
+                        </div>
                         <div class="row mb-3">
                             <!-- <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('Role') }}</label> -->
 
@@ -88,16 +105,12 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="card-footer justify-content-center">
+                        <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Crear Cuenta') }}</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
