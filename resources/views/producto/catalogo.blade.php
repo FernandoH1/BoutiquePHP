e5430176589e14c5b0d5c2d3d101094ccec251a4
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-05-23 22:01:13 -0300

    algunos arreglos en el css

--- resources/views/producto/catalogo.blade.php
+++ resources/views/producto/catalogo.blade.php
@@ -8,12 +8,12 @@ Producto
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-12">
-            <div class="card">
+            <div class="card" style="background: #EBEBEB90">
                 <div class="card-header">
                     <div style="display: flex; justify-content: space-between; align-items: center;">
 
                         <span id="card_title">
-                            {{ __('Catalogo de productos') }}
+                           <b> {{ __('Catalogo de productos') }} </b>
                         </span>
                     </div>
                 </div>
@@ -28,29 +28,21 @@ Producto
                     <form action="{{route('producto.catalogo')}}" method="get">
                         <!-- Barra de busqueda -->
                         <div class="bmd-form-group{{ $errors->has('buscar') ? ' has-danger' : '' }} my-3">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <input type="submit" value="Buscar" class="btn btn-primary">
-                                    </span>
-                                </div>
-                                <input type="text" name="buscar" class="form-control" placeholder="{{ __('Buscar...') }}" value="{{ $texto }}" required>
-                            </div>
+                           
+                        <div class="input-group mb-3">
+                        <input type="text" name="buscar" class="form-control" placeholder="{{ __('Buscar...') }}" value="{{ $texto }}" required>
+                        <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit">Buscar</button>
+                        </div>
+                        </div>
                             @if ($errors->has('buscar'))
                             <div id="buscar-error" class="error text-danger pl-3" for="buscar" style="display: block;">
                                 <strong>{{ $errors->first('buscar') }}</strong>
                             </div>
                             @endif
-
-                            <!-- Check box color -->
-                            <!-- <div class="checkbox checkbox-success">
-                                <input name="chk_color" id="chk_color" type="checkbox" value="chk_color">
-                                <label for="chk_color" style="padding-left: 15px!important;">Color</label>
-                            </div> -->
-
-
+                            <div class="btn-group">
                             <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <button class="btn btn-secondary dropdown-toggle m-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                    Talles
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -63,42 +55,80 @@ Producto
                                     <li><a class="dropdown-item" href="{{route('producto.xxxl')}}">XXXL</a></li>
                                 </ul>
                             </div>
-
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle m-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Color
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><a class="dropdown-item" href="{{route('producto.xs')}}">XS</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.s')}}">S</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.m')}}">M</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.l')}}">L</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xl')}}">XL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxl')}}">XXL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxxl')}}">XXXL</a></li>
+                                </ul>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle m-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                   Marca
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><a class="dropdown-item" href="{{route('producto.xs')}}">XS</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.s')}}">S</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.m')}}">M</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.l')}}">L</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xl')}}">XL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxl')}}">XXL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxxl')}}">XXXL</a></li>
+                                </ul>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle m-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                   Precio
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><a class="dropdown-item" href="{{route('producto.xs')}}">XS</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.s')}}">S</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.m')}}">M</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.l')}}">L</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xl')}}">XL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxl')}}">XXL</a></li>
+                                    <li><a class="dropdown-item" href="{{route('producto.xxxl')}}">XXXL</a></li>
+                                </ul>
+                            </div>
+                            </div>
                         </div>
                         <!-- Barra de busqueda -->
 
                         <!-- Card con las imagenes -->
-                        @if(count($productos)<=0) <div class="card my-5" style="width: 12rem;">
+                        @if(count($productos)<=0) 
+                        <div class="card my-5" style="width: 20rem; margin:auto">
                             <img src="{{ asset('/img/boutique.png') }}" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">No se encontro pordictos similares</h5>
                                 <p class="card-text">Pruebe colocando otro Tipo de producto o con otra Marca</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="{{route('producto.catalogo')}}" class="btn btn-primary">Volver al Incio</a>
                             </div>
                 </div>
                 @else
+                <div class="row" style="margin: auto" >
                 @foreach ($productos as $producto)
-                <div class="card" style="width: 12rem;">
-                    <img src="{{asset('storage').'/'.$producto->foto}}" class="card-img-top" alt="...">
+                <div class="col-3">
+                <div class="card text-black mb-3" style="width: 19rem; margin:auto; margin-top: 20px; background: white; box-shadow: 5px 5px 5px 4px rgba(0, 0, 0, 30%);">
+                    <img src="{{asset('storage').'/'.$producto->foto}}" class="card-img-top" alt="..." height="250px" style="padding: 20px">
                     <div class="card-body">
                         <h5 class="card-title">{{$producto->tipo}} {{$producto->marca}}</h5>
                         <p class="card-text">Talle: {{$producto->talle}} </p>
                         <p class="card-text">Categoria: {{$producto->categoria}} </p>
                         <p class="card-text">Precio: ${{$producto->precio}} </p>
-                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
                     </div>
                 </div>
+                </div>
                 @endforeach
+                </div>
                 @endif
-
-                <!-- {{$productos->links()}} -->
-                <!-- Card con las imagenes -->
                 </form>
-
-
-
-
-
             </div>
         </div>
         {!! $productos->links() !!}
